c9401c1050b33d185804fd7574519fab736c0fb5
oubellaismail <i.oubella1739@uca.ac.ma>
2023-12-23 10:51:23 +0100

    add Daoimpl folder with person full exmaple

--- src/main/java/DAO/PersonDAO.java
+++ src/main/java/DAO/PersonDAO.java
@@ -1,4 +1,10 @@
 package DAO;
-
+import model.Person;
+import java.util.*;
 public interface PersonDAO {
+    public void insertPerson(Person person);
+    public Person findPerson(int id);
+    public List<Person> findAll();
+    public void updatePerson(Person person);
+    public void deletePerson(int id);
 }
--- src/main/java/DAO/QuestionDAO.java
+++ src/main/java/DAO/QuestionDAO.java
@@ -1,5 +1,10 @@
 package DAO;
-
+import java.util.*;
+import model.Question;
 public interface QuestionDAO {
-
+    public void insertQuestion(Question question);
+    public Question findQuestion(int id);
+    public List<Question> findAll();
+    public void updateQuestion(Question question);
+    public void deleteQuestion(int id);
 }
