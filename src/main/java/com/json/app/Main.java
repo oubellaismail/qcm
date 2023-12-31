1545203ac92c09ac2ab16110ada98731542e2866
oubellaismail <i.oubella1739@uca.ac.ma>
2023-12-31 20:12:12 +0100

    level test !

--- src/main/java/com/json/app/Main.java
+++ src/main/java/com/json/app/Main.java
@@ -16,7 +16,7 @@ import com.json.DAOimpl.*;
 public class Main {
     public static void main(String[] args) {
 
-        int count = 1;
+        int count = 0;
         QuizDAO quizDAO = new QuizDAOimp();
         QuestionDAO questionDAO = new QuestionDAOimp();
 
@@ -24,78 +24,93 @@ public class Main {
         if (count == 0) {
             
             try {
-                String apiUrl = "https://quizapi.io/api/v1/questions?apiKey=ktpARm1lcXvIQel8yfOQSWXLMyNe0LKLhdgpMhlG&limit=1";
-        
-                // curl https://quizapi.io/api/v1/questions -G \
-                // -d apiKey=YOUR_API_KEY \
-                // -d limit=10 \
-                // -d category=Linux \
-                // -d difficulty=easy
-                // Create a URI object
-                URI uri = URI.create(apiUrl);
-                // Create an HttpClient
-                HttpClient client = HttpClient.newHttpClient();
-                // Create an HttpRequest
-                HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
-                // Send the request and get the response
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                // Print the response body
-                System.out.println(response.body());
-                String json = response.body();
-        
-                // Use Gson to parse the JSON string into a list of Quiz objects
-                Gson gson = new Gson();
-                Type questionListType = new TypeToken<List<Question>>() {}.getType();
-                List<Question> questionzes = gson.fromJson(json, questionListType);
-        
-                // Now you can work with the list of Quiz objects
-                for (Question question : questionzes) {
-                    System.out.println("*********************************");
-                    System.err.println("id :" + question.getId());
-                    System.err.println("question :" + question.getQuestion());
-                    System.err.println("description :" + question.getDescription());
-        
-                    System.err.println("Answers : ");
-                    System.err.println("answer_a : " + question.getAnswers().getAnswer_a());
-                    System.err.println("answer_b : " + question.getAnswers().getAnswer_b());
-                    System.err.println("answer_c : " + question.getAnswers().getAnswer_c());
-                    System.err.println("answer_d : " + question.getAnswers().getAnswer_d());
-                    System.err.println("answer_e : " + question.getAnswers().getAnswer_e());
-                    System.err.println("answer_f : " + question.getAnswers().getAnswer_f());
+
+                int level = 2;
+                String difficulty ="";
+                
+                for (int i = 0; i < 3; i++) {
+                    switch (i) {
+                        case 0: difficulty = "easy";
+                            break;
+                        case 1: difficulty = "medium";
+                            break;
+                        case 2: difficulty = "hard";
+                            break;
+                    }
+                
+
+                    String apiUrl = "https://quizapi.io/api/v1/questions?apiKey=ktpARm1lcXvIQel8yfOQSWXLMyNe0LKLhdgpMhlG&limit=" + Integer.toString(level) + "&difficulty=" + difficulty;
                     
-                    System.err.println("Multiple correct answers : " + question.getMultiple_correct_answers());
+                    // curl https://quizapi.io/api/v1/questions -G \
+                    // -d apiKey=YOUR_API_KEY \
+                    // -d limit=10 \
+                    // -d category=Linux \
+                    // -d difficulty=easy
+                    // Create a URI object
+                    URI uri = URI.create(apiUrl);
+                    // Create an HttpClient
+                    HttpClient client = HttpClient.newHttpClient();
+                    // Create an HttpRequest
+                    HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+                    // Send the request and get the response
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    // Print the response body
+                    // System.out.println(response.body());
+                    String json = response.body();
+                
+                    // Use Gson to parse the JSON string into a list of Quiz objects
+                    Gson gson = new Gson();
+                    Type questionListType = new TypeToken<List<Question>>() {}.getType();
+                    List<Question> questionzes = gson.fromJson(json, questionListType);
+                
+                    // Now you can work with the list of Quiz objects
+                    for (Question question : questionzes) {
+                        System.out.println("####################################################################");
+                        System.err.println("id :" + question.getId());
+                        System.err.println("question :" + question.getQuestion());
+                        System.err.println("description :" + question.getDescription());
+                        
+                        System.err.println("Answers : ");
+                        System.err.println("answer_a : " + question.getAnswers().getAnswer_a());
+                        System.err.println("answer_b : " + question.getAnswers().getAnswer_b());
+                        System.err.println("answer_c : " + question.getAnswers().getAnswer_c());
+                        System.err.println("answer_d : " + question.getAnswers().getAnswer_d());
+                        System.err.println("answer_e : " + question.getAnswers().getAnswer_e());
+                        System.err.println("answer_f : " + question.getAnswers().getAnswer_f());
+                        System.err.println("Multiple correct answers : " + question.getMultiple_correct_answers());
+                        System.err.println("Correct Answers : ");
+                        System.err.println("answer_a_correct : " + question.getCorrect_answers().getAnswer_a_correct());
+                        System.err.println("answer_b_correct : " + question.getCorrect_answers().getAnswer_b_correct());
+                        System.err.println("answer_c_correct : " + question.getCorrect_answers().getAnswer_c_correct());
+                        System.err.println("answer_d_correct : " + question.getCorrect_answers().getAnswer_d_correct());
+                        System.err.println("answer_e_correct : " + question.getCorrect_answers().getAnswer_e_correct());
+                        System.err.println("answer_f_correct : " + question.getCorrect_answers().getAnswer_f_correct());
+                        
+                        System.err.println("Correct answers : " + question.getCorrect_answer());
+                        System.err.println("Explanation : " + question.getExplanation());
+                        System.err.println("tip : " + question.getTip());
+                        System.err.println("Tag : " + question.getTags().get(0).getName());
+                        System.err.println("Category : " + question.getCategory());
+                        System.err.println("Defificulty : " + question.getDifficulty());
+                        System.out.println("####################################################################");
+                        
+                
+                        // System.out.println("*********************************");
+                        // System.err.println(question);
+                        // System.out.println("*********************************");
                     
-                    System.err.println("Correct Answers : ");
-                    System.err.println("answer_a_correct : " + question.getCorrect_answers().getAnswer_a_correct());
-                    System.err.println("answer_b_correct : " + question.getCorrect_answers().getAnswer_b_correct());
-                    System.err.println("answer_c_correct : " + question.getCorrect_answers().getAnswer_c_correct());
-                    System.err.println("answer_d_correct : " + question.getCorrect_answers().getAnswer_d_correct());
-                    System.err.println("answer_e_correct : " + question.getCorrect_answers().getAnswer_e_correct());
-                    System.err.println("answer_f_correct : " + question.getCorrect_answers().getAnswer_f_correct());
-        
-                    System.err.println("Correct answers : " + question.getCorrect_answer());
-                    System.err.println("Explanation : " + question.getExplanation());
-                    System.err.println("tip : " + question.getTip());
-                    System.err.println("Tag : " + question.getTags().get(0).getName());
-                    System.err.println("Category : " + question.getCategory());
-                    System.err.println("Defificulty : " + question.getDifficulty());
-        
-        
-                    System.out.println("*********************************");
-                    System.err.println(question);
-                    System.out.println("*********************************");
-        
+                    }
+                
+                
+                        System.out.println("****************************************************************************************************************");
+                        // quizDAO.insertQuiz(new Quiz (questionzes));
                 }
-        
-        
-                System.out.println("****************************************************************************************************************");
-                quizDAO.insertQuiz(new Quiz (questionzes));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            }       
+            
+            catch (Exception e) {
+                    e.printStackTrace();
+            }   
         }
-
         else {
             quizDAO.deleteQuiz(5);
         }
