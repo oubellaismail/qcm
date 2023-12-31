f5eabcca28cc2afbc38ea61ea71272d7e8d5cea4
oubellaismail <i.oubella1739@uca.ac.ma>
2023-12-31 23:25:52 +0100

    module-info update !

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module controller.qcmpro {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires com.google.gson;
+    requires java.net.http;
 
     opens controller.qcmpro to javafx.fxml;
     exports controller.qcmpro;
