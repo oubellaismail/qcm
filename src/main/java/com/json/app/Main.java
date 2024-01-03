37f3c42726c35aa26717cda6a39c2bcc4c28869f
oubellaismail <i.oubella1739@uca.ac.ma>
2024-01-03 13:55:03 +0100

    main class

--- src/main/java/com/json/app/Main.java
+++ src/main/java/com/json/app/Main.java
@@ -1,104 +1,77 @@
 package com.json.app;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import com.json.DAO.*;
+import com.json.DAOimpl.*;
+import com.json.model.*;
+
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
-import java.lang.reflect.Type;
 import java.util.List;
-import com.json.model.*;
-import com.json.DAO.*;
-import com.json.DAOimpl.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        int count = 1;
         QuizDAO quizDAO = new QuizDAOimp();
-        QuestionDAO questionDAO = new QuestionDAOimp();
+        UserDAO userDAO = new UserDAOimp();
+        userDAO.inserUser(new User("ismail.code", "ismail@mail.com", "just"));
+        User user = userDAO.findUser(new User("ismail@mail.com", "just"));
+        Quiz quiz = new Quiz(user.getId());
+        quizDAO.insertQuiz(quiz);
+        int count = 0;
 
-
-        if (count == 0) {
-            
+        if (user != null) {
             try {
-                String apiUrl = "https://quizapi.io/api/v1/questions?apiKey=ktpARm1lcXvIQel8yfOQSWXLMyNe0LKLhdgpMhlG&limit=1";
-        
-                // curl https://quizapi.io/api/v1/questions -G \
-                // -d apiKey=YOUR_API_KEY \
-                // -d limit=10 \
-                // -d category=Linux \
-                // -d difficulty=easy
-                // Create a URI object
-                URI uri = URI.create(apiUrl);
-                // Create an HttpClient
-                HttpClient client = HttpClient.newHttpClient();
-                // Create an HttpRequest
-                HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
-                // Send the request and get the response
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                // Print the response body
-                System.out.println(response.body());
-                String json = response.body();
-        
-                // Use Gson to parse the JSON string into a list of Quiz objects
-                Gson gson = new Gson();
-                Type questionListType = new TypeToken<List<Question>>() {}.getType();
-                List<Question> questionzes = gson.fromJson(json, questionListType);
-        
-                // Now you can work with the list of Quiz objects
-                for (Question question : questionzes) {
-                    System.out.println("*********************************");
-                    System.err.println("id :" + question.getId());
-                    System.err.println("question :" + question.getQuestion());
-                    System.err.println("description :" + question.getDescription());
-        
-                    System.err.println("Answers : ");
-                    System.err.println("answer_a : " + question.getAnswers().getAnswer_a());
-                    System.err.println("answer_b : " + question.getAnswers().getAnswer_b());
-                    System.err.println("answer_c : " + question.getAnswers().getAnswer_c());
-                    System.err.println("answer_d : " + question.getAnswers().getAnswer_d());
-                    System.err.println("answer_e : " + question.getAnswers().getAnswer_e());
-                    System.err.println("answer_f : " + question.getAnswers().getAnswer_f());
-                    
-                    System.err.println("Multiple correct answers : " + question.getMultiple_correct_answers());
-                    
-                    System.err.println("Correct Answers : ");
-                    System.err.println("answer_a_correct : " + question.getCorrect_answers().getAnswer_a_correct());
-                    System.err.println("answer_b_correct : " + question.getCorrect_answers().getAnswer_b_correct());
-                    System.err.println("answer_c_correct : " + question.getCorrect_answers().getAnswer_c_correct());
-                    System.err.println("answer_d_correct : " + question.getCorrect_answers().getAnswer_d_correct());
-                    System.err.println("answer_e_correct : " + question.getCorrect_answers().getAnswer_e_correct());
-                    System.err.println("answer_f_correct : " + question.getCorrect_answers().getAnswer_f_correct());
-        
-                    System.err.println("Correct answers : " + question.getCorrect_answer());
-                    System.err.println("Explanation : " + question.getExplanation());
-                    System.err.println("tip : " + question.getTip());
-                    System.err.println("Tag : " + question.getTags().get(0).getName());
-                    System.err.println("Category : " + question.getCategory());
-                    System.err.println("Defificulty : " + question.getDifficulty());
-        
-        
-                    System.out.println("*********************************");
-                    System.err.println(question);
-                    System.out.println("*********************************");
-        
-                }
-        
-        
-                System.out.println("****************************************************************************************************************");
-                quizDAO.insertQuiz(new Quiz (questionzes));
 
-            } catch (Exception e) {
+                int level = 1;
+
+                for (String difficulty : List.of("easy", "medium", "hard")) {
+
+                    String apiUrl = "https://quizapi.io/api/v1/questions?apiKey=ktpARm1lcXvIQel8yfOQSWXLMyNe0LKLhdgpMhlG&limit=" 
+                            + Integer.toString(level) + "&difficulty=" + difficulty;
+
+                    // curl https://quizapi.io/api/v1/questions -G \
+                    // -d apiKey=YOUR_API_KEY \
+                    // -d limit=10 \
+                    // -d category=Linux \
+                    // -d difficulty=easy
+                    // Create a URI object
+                    URI uri = URI.create(apiUrl);
+                    // Create an HttpClient
+                    HttpClient client = HttpClient.newHttpClient();
+                    // Create an HttpRequest
+                    HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+                    // Send the request and get the response
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    // Print the response body
+                    // System.out.println(response.body());
+                    String json = response.body();
+
+                    // Use Gson to parse the JSON string into a list of Quiz objects
+                    Gson gson = new Gson();
+                    Type questionListType = new TypeToken<List<Question>>() {
+                    }.getType();
+                    List<Question> questionzes = gson.fromJson(json, questionListType);
+                    quiz.getQuestions().addAll(questionzes);
+                    quizDAO.updateQuiz(quiz, count++ * level);
+                }
+            }
+
+            catch (Exception e) {
                 e.printStackTrace();
             }
+        } else {
+            // System.out.print(quizDAO.findQuiz(12));
+            // System.out.println("User not found.");
+            // System.out.println(quizDAO.findQuiz(4));
+            // System.out.println(quizDAO.findAll(1));
+            // quizDAO.deleteQuiz(11);
+            // userDAO.deleteUser(1);
+            System.out.println("No user !");
         }
-
-        else {
-            quizDAO.deleteQuiz(5);
-        }
-
     }
 }
